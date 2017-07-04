d0e72e76d217a53357d2318c14632533db13d088
trf <trf@ubuntu.ubuntu-domain>
2017-07-04 05:17:26 -0400

    so

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-config.assets.initialize_on_precompile = false
+
 
 require_relative 'boot'
 
@@ -9,8 +9,9 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Dream
+module Passen
   class Application < Rails::Application
+  	config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
